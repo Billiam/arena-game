7cb62a21392d926e9f7e591336b7d74ff8974a86
Billiam <billiamthesecond@gmail.com>
2015-01-18 12:30:09 -0600

    Adding resource reloading to fix caching vs live reload.

--- resource.lua
+++ resource.lua
@@ -1,9 +1,16 @@
 local Proxy = require('vendor.proxy')
 local Resources = {}
 
-Resources.image = Proxy(function(k) return assert(love.graphics.newImage('assets/img/' .. k .. '.png')) end)
-Resources.state = Proxy(function(k) return assert(love.filesystem.load('states/' .. k .. '.lua'))() end)
-Resources.view = Proxy(function(k) return assert(love.filesystem.load('view/' .. k .. '.lua'))() end)
-Resources.audio = Proxy(function(k) return love.audio.newSource('assets/audio/' .. k .. '.ogg', 'static') end)
+function Resources.init()
+  Resources.image = Proxy(function(k) return assert(love.graphics.newImage('assets/img/' .. k .. '.png')) end)
+  Resources.state = Proxy(function(k) return assert(love.filesystem.load('states/' .. k .. '.lua'))() end)
+  Resources.view = Proxy(function(k) return assert(love.filesystem.load('view/' .. k .. '.lua'))() end)
+  Resources.audio = Proxy(function(k) return love.audio.newSource('assets/audio/' .. k .. '.ogg', 'static') end)
+end
 
+function Resources.reload()
+  Resources.init()
+end
+
+Resources.init()
 return Resources
