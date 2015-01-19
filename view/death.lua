f1162f6984b8943ce75a29509a00f579944ac7a5
Billiam <billiamthesecond@gmail.com>
2015-01-19 01:09:23 -0600

    Centering text in death and pause states.

--- view/death.lua
+++ view/death.lua
@@ -5,7 +5,7 @@ function Death.render()
   love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
   
   love.graphics.setColor(255, 255, 255, 255)
-  love.graphics.print("The end", 300, 300)
+  love.graphics.printf("The End", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), 'center')
 end
 
 return Death
--- view/pause.lua
+++ view/pause.lua
@@ -2,7 +2,7 @@ local Pause = {}
 
 function Pause.render()
   love.graphics.setColor(255, 255, 255, 255)
-  love.graphics.print("Pause screen", 300, 300)
+  love.graphics.printf("Pause screen", 0, love.graphics.getHeight()/2, love.graphics.getWidth(), 'center')
 end
 
 return Pause
